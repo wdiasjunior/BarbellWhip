ff89056f8b4b715972364ce8d670bfad2c78ec6a
Wellington Junior <wdiasjunior@gmail.com>
2023-09-10 19:47:02 -0300

    download and open apk file logic done

--- src/sharedComponents/updateModal/updateModal.tsx
+++ src/sharedComponents/updateModal/updateModal.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, NativeModules } from "react-native";
 import Modal from "react-native-modal";
 
 import RNFS from 'react-native-fs';
@@ -20,6 +20,9 @@ const UpdateModal = (props: Props) => {
 
   const activeTheme = useAtomValue(activeThemeAtom);
 
+  const { FileOpenerModule } = NativeModules;
+
+
   const url = `https://github.com/wdiasjunior/BarbellWhip/releases/download/v${props.appVersionGithub}/BarbellWhip_${props.appVersionGithub}.apk`
   const filePath = `${RNFS.ExternalCachesDirectoryPath}/BarbellWhip_${props.appVersionGithub}.apk`;
 
@@ -29,26 +32,31 @@ const UpdateModal = (props: Props) => {
   // - download to temp?
   //   - https://github.com/joltup/rn-fetch-blob
 
-  useEffect(() => {
-    // Optional: Delete the file if it exists before downloading
-    RNFS.unlink(filePath)
-      .then(() => console.log('Previous file deleted'))
-      .catch((err) => console.log(err.message));
-  }, [props.appVersionGithub])
+  // useEffect(() => {
+  //   // Optional: Delete the file if it exists before downloading
+  //   RNFS.unlink(filePath)
+  //     .then(() => console.log('Previous file deleted'))
+  //     .catch((err) => console.log(err.message));
+  // }, [props.appVersionGithub])
 
   const handleDownload = () => {
-    RNFS.downloadFile({
-      fromUrl: url,
-      toFile: filePath,
-      progress: (res) => {
-        // Handle download progress updates if needed
-        const progress = (res.bytesWritten / res.contentLength) * 100;
-        console.log(`Progress: ${progress.toFixed(2)}%`);
-      },
-    })
-      .promise.then((response) => console.log('File downloaded!', response))
-      .catch((err) => console.log('Download error:', err));
+    // RNFS.downloadFile({
+    //   fromUrl: url,
+    //   toFile: filePath,
+    //   progress: (res) => {
+    //     // Handle download progress updates if needed
+    //     const progress = (res.bytesWritten / res.contentLength) * 100;
+    //     console.log(`Progress: ${progress.toFixed(2)}%`);
+    //   },
+    // })
+    //   .promise.then((response) => console.log('File downloaded!', response))
+    //   .catch((err) => console.log('Download error:', err));
   }
+
+  const handleInstallAPK = () => {
+    FileOpenerModule.openFile(filePath);
+  }
+console.log(parseFloat(props.currentVersion), parseFloat(props.appVersionGithub));
 
   return (
     <Modal
@@ -66,12 +74,19 @@ const UpdateModal = (props: Props) => {
         <Text style={styles(activeTheme).text}>UpdateModal</Text>
         <Text style={styles(activeTheme).text}>{props.currentVersion}</Text>
         <Text style={styles(activeTheme).text}>{props.appVersionGithub}</Text>
+        <Text style={styles(activeTheme).text}>{parseFloat(props.currentVersion) < parseFloat(props.appVersionGithub) ? "update available" : "no update available"}</Text>
 
         <TouchableOpacity
           style={styles(activeTheme).updateCheckerButton}
           onPress={() => handleDownload()}
         >
           <Text style={styles(activeTheme).text}>Download update</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles(activeTheme).updateCheckerButton}
+          onPress={() => handleInstallAPK()}
+        >
+          <Text style={styles(activeTheme).text}>Install update</Text>
         </TouchableOpacity>
       </View>
     </Modal>
