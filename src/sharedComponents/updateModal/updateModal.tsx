bc7f4fb86d61c6aaee0fb36a0c8018fec3ba415e
Wellington Junior <wdiasjunior@gmail.com>
2023-09-10 00:40:43 -0300

    download from github releases works

--- src/sharedComponents/updateModal/updateModal.tsx
+++ src/sharedComponents/updateModal/updateModal.tsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 import Modal from "react-native-modal";
+
+import RNFS from 'react-native-fs';
 
 import styles from "./updateModalStyles";
 
@@ -18,14 +20,34 @@ const UpdateModal = (props: Props) => {
 
   const activeTheme = useAtomValue(activeThemeAtom);
 
+  const url = `https://github.com/wdiasjunior/BarbellWhip/releases/download/v${props.appVersionGithub}/BarbellWhip_${props.appVersionGithub}.apk`
+  const filePath = `${RNFS.ExternalCachesDirectoryPath}/BarbellWhip_${props.appVersionGithub}.apk`;
+
   // TODO
   // - add spinner on request
   // - "dont show again" option on auto check
   // - download to temp?
   //   - https://github.com/joltup/rn-fetch-blob
 
-  const handleDownloadUpdate = () => {
-    const url = `https://github.com/wdiasjunior/BarbellWhip/releases/download/v${appVersionGithub}/BarbellWhip_${appVersionGithub}.apk`
+  useEffect(() => {
+    // Optional: Delete the file if it exists before downloading
+    RNFS.unlink(filePath)
+      .then(() => console.log('Previous file deleted'))
+      .catch((err) => console.log(err.message));
+  }, [props.appVersionGithub])
+
+  const handleDownload = () => {
+    RNFS.downloadFile({
+      fromUrl: url,
+      toFile: filePath,
+      progress: (res) => {
+        // Handle download progress updates if needed
+        const progress = (res.bytesWritten / res.contentLength) * 100;
+        console.log(`Progress: ${progress.toFixed(2)}%`);
+      },
+    })
+      .promise.then((response) => console.log('File downloaded!', response))
+      .catch((err) => console.log('Download error:', err));
   }
 
   return (
@@ -44,6 +66,13 @@ const UpdateModal = (props: Props) => {
         <Text style={styles(activeTheme).text}>UpdateModal</Text>
         <Text style={styles(activeTheme).text}>{props.currentVersion}</Text>
         <Text style={styles(activeTheme).text}>{props.appVersionGithub}</Text>
+
+        <TouchableOpacity
+          style={styles(activeTheme).updateCheckerButton}
+          onPress={() => handleDownload()}
+        >
+          <Text style={styles(activeTheme).text}>Download update</Text>
+        </TouchableOpacity>
       </View>
     </Modal>
   );
