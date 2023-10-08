7334deeca3e02be64e7f7a3ba88cddb1ad24e1d1
Wellington Junior <wdiasjunior@gmail.com>
2023-10-08 20:46:11 -0300

    rename update checker helper function

--- src/helpers/updateChecker.ts
+++ src/helpers/updateChecker.ts
@@ -1,4 +1,8 @@
-export function hasUpdateAvailable(installedVersion: string, lastestVersion: string): boolean {
+export function updateChecker(installedVersion: string, lastestVersion: string): boolean {
+  if(!installedVersion || !lastestVersion) {
+    return false;
+  }
+
   const installedVersionParts = installedVersion.split('.').map(Number);
   const lastestVersionParts = lastestVersion.split('.').map(Number);
 
